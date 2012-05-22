7f20f2c7fd85184b8da85f11c4ab68eee361fa60
Steve Richert <steve.richert@gmail.com>
2012-05-22 14:00:59 -0400

    Isolate the paths and selectors module tests from the top-level Cucumber::Web namespace

--- spec/cucumber/web/paths_spec.rb
+++ spec/cucumber/web/paths_spec.rb
@@ -1,52 +1,58 @@
 require 'spec_helper'
 
 describe Cucumber::Web::Paths do
+  subject do
+    mod = Module.new
+    mod.extend Cucumber::Web::Paths
+    mod
+  end
+
   describe '.paths' do
     context 'when given a block' do
       it 'defines string to string paths' do
-        Cucumber::Web.paths do
+        subject.paths do
           define('the homepage', '/')
         end
-        Cucumber::Web.paths.should == [['the homepage', '/']]
+        subject.paths.should == [['the homepage', '/']]
       end
 
       it 'defines string to proc paths' do
         block = proc{ '/' }
-        Cucumber::Web.paths do
+        subject.paths do
           define('the homepage', block)
         end
-        Cucumber::Web.paths.should == [['the homepage', block]]
+        subject.paths.should == [['the homepage', block]]
       end
 
       it 'defines string to block paths' do
         block = proc{ '/' }
-        Cucumber::Web.paths do
+        subject.paths do
           define('the homepage', &block)
         end
-        Cucumber::Web.paths.should == [['the homepage', block]]
+        subject.paths.should == [['the homepage', block]]
       end
 
       it 'defines regular expression to string paths' do
-        Cucumber::Web.paths do
+        subject.paths do
           define(/^the home ?page$/, '/')
         end
-        Cucumber::Web.paths.should == [[/^the home ?page$/, '/']]
+        subject.paths.should == [[/^the home ?page$/, '/']]
       end
 
       it 'defines regular expression to proc paths' do
         block = proc{ '/' }
-        Cucumber::Web.paths do
+        subject.paths do
           define(/^the home ?page$/, block)
         end
-        Cucumber::Web.paths.should == [[/^the home ?page$/, block]]
+        subject.paths.should == [[/^the home ?page$/, block]]
       end
 
       it 'defines regular expression to block paths' do
         block = proc{ '/' }
-        Cucumber::Web.paths do
+        subject.paths do
           define(/^the home ?page$/, &block)
         end
-        Cucumber::Web.paths.should == [[/^the home ?page$/, block]]
+        subject.paths.should == [[/^the home ?page$/, block]]
       end
     end
   end
--- spec/cucumber/web/selectors_spec.rb
+++ spec/cucumber/web/selectors_spec.rb
@@ -1,52 +1,58 @@
 require 'spec_helper'
 
 describe Cucumber::Web::Selectors do
+  subject do
+    mod = Module.new
+    mod.extend Cucumber::Web::Selectors
+    mod
+  end
+
   describe '.selectors' do
     context 'when given a block' do
       it 'defines string to string selectors' do
-        Cucumber::Web.selectors do
+        subject.selectors do
           define('the page', 'html > body')
         end
-        Cucumber::Web.selectors.should == [['the page', 'html > body']]
+        subject.selectors.should == [['the page', 'html > body']]
       end
 
       it 'defines string to proc selectors' do
         block = proc{ 'html > body' }
-        Cucumber::Web.selectors do
+        subject.selectors do
           define('the page', block)
         end
-        Cucumber::Web.selectors.should == [['the page', block]]
+        subject.selectors.should == [['the page', block]]
       end
 
       it 'defines string to block selectors' do
         block = proc{ 'html > body' }
-        Cucumber::Web.selectors do
+        subject.selectors do
           define('the page', &block)
         end
-        Cucumber::Web.selectors.should == [['the page', block]]
+        subject.selectors.should == [['the page', block]]
       end
 
       it 'defines regular expression to string selectors' do
-        Cucumber::Web.selectors do
+        subject.selectors do
           define(/^the page$/, 'html > body')
         end
-        Cucumber::Web.selectors.should == [[/^the page$/, 'html > body']]
+        subject.selectors.should == [[/^the page$/, 'html > body']]
       end
 
       it 'defines regular expression to proc selectors' do
         block = proc{ 'html > body' }
-        Cucumber::Web.selectors do
+        subject.selectors do
           define(/^the page$/, block)
         end
-        Cucumber::Web.selectors.should == [[/^the page$/, block]]
+        subject.selectors.should == [[/^the page$/, block]]
       end
 
       it 'defines regular expression to block selectors' do
         block = proc{ 'html > body' }
-        Cucumber::Web.selectors do
+        subject.selectors do
           define(/^the page$/, &block)
         end
-        Cucumber::Web.selectors.should == [[/^the page$/, block]]
+        subject.selectors.should == [[/^the page$/, block]]
       end
     end
   end
