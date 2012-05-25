e9d5390e3513e26e4f4e004ee56dc4ec9a4d1478
Steve Richert <steve.richert@gmail.com>
2012-05-25 16:03:47 -0400

    Modules are tested in isolation so there's no need to clean up the Cucumber::Web namespace after specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
 require 'cucumber/web'
 
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each{|f| require f }
-
-RSpec.configure do |config|
-  config.after do
-    Cucumber::Web.instance_variable_set(:@paths_proxy, nil)
-    Cucumber::Web.instance_variable_set(:@selectors_proxy, nil)
-  end
-end
