7fbdee5195dc006a6222a3e540da17bc9e74c0dd
Steve Richert <steve.richert@gmail.com>
2012-05-29 13:15:38 -0400

    Test that the Cucumber::Web module extends paths, selectors and steps

--- spec/cucumber/web_spec.rb
+++ spec/cucumber/web_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Cucumber::Web do
+  it 'extends paths' do
+    subject.should be_a(Cucumber::Web::Paths)
+  end
+
+  it 'extends selectors' do
+    subject.should be_a(Cucumber::Web::Selectors)
+  end
+
+  it 'extends steps' do
+    subject.should be_a(Cucumber::Web::Steps)
+  end
 end
